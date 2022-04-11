25edfe5636cf8c686d63827346c9f48e11b68852
AnthonyLayne <kepheus.ds@gmail.com>
2022-04-11 15:21:59 -0400

    Passed props to todolist component, listed out initial todos

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -27,7 +27,7 @@ export default class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <TodoList />
+        <TodoList todos={todos} />
         <Form />
       </div>
     );
--- frontend/components/TodoList.js
+++ frontend/components/TodoList.js
@@ -1,7 +1,21 @@
 import React from "react";
 
 export default class TodoList extends React.Component {
+  //Need to pass props so this component has access to state.
   render() {
-    return <div>TodoList:</div>;
+    return (
+      <div>
+        TodoList:
+        <ul>
+          {this.props.todos.map((todo) => {
+            return (
+              <li>
+                {todo.name} {todo.finished ? <span>Finished</span> : <span></span>}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
   }
 }
